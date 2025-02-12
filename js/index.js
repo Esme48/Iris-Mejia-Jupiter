980cda82bf44631c2298d41c1b20f5822b4484f8
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-02-11 21:55:46 -0700

    Changes made to fetch, fetch not displlaying

--- js/index.js
+++ js/index.js
@@ -2,22 +2,27 @@
 const today=new Date()
 const thisYear=today.getFullYear()
 
+
 //Footer Part
 const footer=document.createElement("footer")
 const body=document.querySelector("body")
 body.appendChild(footer)
+
 
 //Copywrite
 const copyright=document.createElement("p")
 copyright.innerHTML=` © I.Mejia ${thisYear}`
 footer.appendChild(copyright)
 
+
 //Skills
 const skills=["Adapt to new technologies", "Manage change and pivot", "C++", "Assembly","HTML", "CSS","JS","AutoDesk Fusion", "Time management","Problem solving","Excellent Communicator"]
 const skillsSection=document.getElementById("skills")
 
+
 //List
 const skillsList=skillsSection.querySelector("ul")
+
 
 for(let i=0; i<skills.length; i++){
   const skill=document.createElement("li")
@@ -25,25 +30,31 @@ for(let i=0; i<skills.length; i++){
   skillsList.appendChild(skill)
 }
 
+
 //The Message Form Stuffffff//
+
 
 function onFormSubmit(event){
   event.preventDefault();
- //Setting up the data
-  const usersName= event.target.usersName;
-  const usersEmail= event.target.usersEmail;
-  const usersMessage= event.target.usersMessage;
+  //Setting up the data
+  const usersName= event.target.usersName.value;
+  const usersEmail= event.target.usersEmail.value;
+  const usersMessage= event.target.usersMessage.value;
   console.log(usersName);
   console.log(usersEmail);
   console.log(usersMessage);
 
+
   const messageSection=document.getElementById("messages");
-  const messageList=messageSection.getElementsByTagName("ul");
+  const messageList=messageSection.querySelector("ul");
   console.log(messageList);
+
 
   messageSection.hidden=false;
 
+
   //Displaying the message
+
 
   const newMessage=document.createElement("li");
   newString= `<a href="mailto:${usersEmail}">${usersName}</a>\n
@@ -51,66 +62,80 @@ function onFormSubmit(event){
   //console.log(newString);
   newMessage.innerHTML.newString;
 
+
   //The Remove Button
   const removeButton=document.createElement("button");
   removeButton.innerText="Remove";
   removeButton.type="button";
   removeButton.addEventListener("click", function (){
-  const entry=this.parentNode;
-  entry.remove();
+    const entry=this.parentNode;
+    entry.remove();
   });
+
 
   //Add a remove button
   newMessage.appendChild(removeButton);
 
+
   //Add a message
   messageList.appendChild(newMessage);
 
+
   event.target.reset();
 
+
 }
+
 
 const messageForms=document.getElementsByName("leave_message");
 //console.log(messageForms);
 const messageForm=messageForms[0];
+
 
 //Message Section Hidden Component
 const messageSection= document.getElementById("messages");
 messageSection.hidden = true;
 //console.log(messageForm);
 
+
 //When submit is pressed, call the onFormSubmit and execute that function
 messageForm.addEventListener("submit", onFormSubmit);
 
+
+
+
+
+
 //Fetch Component, a promise that has our data//
-const projectSection = document.getElementById("projects");
+let projectSection = document.getElementById("projects");
 console.log("projectSection:", projectSection);
-const projectList = projectSection.querySelector("ul");
+let projectList = projectSection.querySelector("ul");
 console.log("projectList", projectList);
+
+
 
 
 fetch("https://api.github.com/users/Esme48/repos")
 .then((response)=>{
   //Body of the callback function
-  return response.json(); 
+  return response.json();
 })
+
+
 .then((repositories)=>{
-console.log("repositories:", repositories);
-for (let i=0; i < repositories.length; i++){
-  const project = repositories[i].name;
-  const li = document.createElement("li");
-  //innertext of the li element
-  li.innerText = project;
-  projectList.appendChild(li);
-}
- })
-
- .catch((error)=>{
-console.log(error);
- });
+  console.log("repositories:", repositories);
+  for (let i=0; i < repositories.length; i++){
+    let project = repositories[i].name;
+    let li = document.createElement("li");
+    //innertext of the li element
+    li.innerText = project;
+    projectList.appendChild(li);
+  }
+})
 
 
-
-
+.catch((error)=>{
+  console.log(error);
+});
 
 
